425546d38ffeb3e39fa166256bdad6b4a75cfd49
Anna <anna.mikhova@gmail.com>
2023-06-12 19:05:57 +0300

    adaptive header

--- Travel Agency project/ClientApp/src/app/header/header.component.ts
+++ Travel Agency project/ClientApp/src/app/header/header.component.ts
@@ -13,12 +13,34 @@ export class HeaderComponent {
   style = {
     display: "none"
   }
+
   show: boolean = true;
   MenuClick(): void {
     let newStyle = { ...this.style };
     newStyle.display = this.show ? "flex" : "none";
     this.style = newStyle;
     this.show = !this.show;
+  }
+  dropDownMenuVisible = false;
+
+  toggleDropDownMenu(): void {
+    this.dropDownMenuVisible = !this.dropDownMenuVisible;
+  }
+
+  dropDownMenuVisibleC = false;
+
+  toggleDropDownMenuC(): void {
+    this.dropDownMenuVisibleC = !this.dropDownMenuVisibleC;
+  }
+  dropDownMenuVisibleS = false;
+
+  toggleDropDownMenuS(): void {
+    this.dropDownMenuVisibleS = !this.dropDownMenuVisibleS;
+  }
+  dropDownMenuVisibleA = false;
+
+  toggleDropDownMenuA(): void {
+    this.dropDownMenuVisibleA = !this.dropDownMenuVisibleA;
   }
 }
 
